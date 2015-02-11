d677ba43688690e4fc4f54921676b693fa9dd65e
orlyn <arrestif@gmail.com>
2015-02-11 17:06:27 +0100

    Modification de la taille de la fenêtre

--- TestSDL/main.c
+++ TestSDL/main.c
@@ -16,13 +16,13 @@ int main(int argc, char** argv)
 		SDL_Window* pWindow = NULL;
 		pWindow = SDL_CreateWindow("Ma premiere application SDL2", SDL_WINDOWPOS_UNDEFINED,
 			SDL_WINDOWPOS_UNDEFINED,
-			640,
-			480,
+			1280,
+			720,
 			SDL_WINDOW_SHOWN);
 
 		if (pWindow)
 		{
-			SDL_Delay(30000); /* Attendre trois secondes, que l'utilisateur voie la fenêtre */
+			SDL_Delay(3000); /* Attendre trois secondes, que l'utilisateur voie la fenêtre */
 
 			SDL_DestroyWindow(pWindow);
 		}
