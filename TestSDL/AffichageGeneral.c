6f0237876970ed4436b7021f1bfa7a025a0c3d6d
Simon Jeanneteau <simon.jeanneteau@insa-rennes.fr>
2015-02-14 20:17:46 +0100

    Worms Picture Add

--- TestSDL/AffichageGeneral.c
+++ TestSDL/AffichageGeneral.c
@@ -1,22 +1,7 @@
 #include <stdio.h>
 #include "AffichageGeneral.h" /* Inclus la SDL.h */
 
-int afficheImage(SDL_Window *pWindow)
-{
-	//int *x = NULL, *y = NULL;
 
-	SDL_Surface* imageTest = SDL_LoadBMP("../assets/hello.bmp");
-	if (imageTest == NULL)   /*test loadBmp image*/
-	{
-		printf("Unable to load bitmap: %s\n", SDL_GetError());
-		return -1;
-	};
-
-	//SDL_GetWindowSize(pWindow, x, y);
-	SDL_Rect dest = { 1080 / 2 - imageTest->w / 2, 600 / 2 - imageTest->h / 2, 0, 0 };
-	SDL_BlitSurface(imageTest, NULL, SDL_GetWindowSurface(pWindow), &dest);
-	SDL_UpdateWindowSurface(pWindow);
-}
 
 void mainFenetre(){
 
@@ -35,7 +20,7 @@ void mainFenetre(){
 		pWindow = creerFenetre(1080, 600, "KaamWorms");
 
 		if (pWindow != NULL){
-			afficheImage(pWindow);
+			afficheImage(pWindow,"../assets/worms_left.bmp");
 			while (quit != 1){ /* continue tant qu'on ne clique pas sur la croix !! */
 				SDL_UpdateWindowSurface(pWindow);
 				while (SDL_PollEvent(&event)) /* Récupération des actions de l'utilisateur */
@@ -88,4 +73,18 @@ SDL_Window * creerFenetre(const int x, const int y, const char * nom){
 		return NULL;
 	}
 	else return pWindow;
+}
+int afficheImage(SDL_Window *pWindow,const char * file)
+{
+	SDL_Surface* imageTest = SDL_LoadBMP(file);
+	if (imageTest == NULL)   /*test loadBmp image*/
+	{
+		printf("Unable to load bitmap: %s\n", SDL_GetError());
+		return -1;
+	};
+
+	//SDL_GetWindowSize(pWindow, x, y);
+	SDL_Rect dest = { 1080 / 2 - imageTest->w / 2, 600 / 2 - imageTest->h / 2, 0, 0 };
+	SDL_BlitSurface(imageTest, NULL, SDL_GetWindowSurface(pWindow), &dest);
+	SDL_UpdateWindowSurface(pWindow);
 }
--- TestSDL/AffichageGeneral.h
+++ TestSDL/AffichageGeneral.h
@@ -1,5 +1,5 @@
 #include <SDL/SDL.h>
 
-int afficheImage(SDL_Window *pWindow);
+int afficheImage(SDL_Window *pWindow, const char * file);
 void mainFenetre();
 SDL_Window * creerFenetre(const int x, const int y, const char * nom);
