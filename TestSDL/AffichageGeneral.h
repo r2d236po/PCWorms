aaacaa2e1376d6ab4c79d35709bb5046be407132
orlyn44 <arrestif@gmail.com>
2015-02-16 14:39:42 +0100

    Commentaires + remise en forme

--- TestSDL/AffichageGeneral.h
+++ TestSDL/AffichageGeneral.h
@@ -1,6 +1,6 @@
 #include <SDL/SDL.h>
 
 int afficheImage(SDL_Window *pWindow, SDL_Surface * image); /*TODO: réecrire les protos*/
-void mainFenetre();
+int mainFenetre();
 SDL_Surface * loadImage(const char * file);
-SDL_Window * creerFenetre(const int x, const int y, const char * nom);
+SDL_Window * creerFenetre(const int w, const int h, const char * nom);
