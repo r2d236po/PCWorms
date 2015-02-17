9efe571396f19fbf75e3c41f7db86961d788c485
orlyn44 <arrestif@gmail.com>
2015-02-17 21:00:23 +0100

    réparation projet

--- TestSDL/AffichageGeneral.h
+++ TestSDL/AffichageGeneral.h
@@ -8,7 +8,7 @@
 int afficheImage(SDL_Window *pWindow, SDL_Surface * image); /*TODO: réecrire les protos*/
 int mainFenetre();
 int mainFenetre2(); //main de test pour le renderer
-void AfficherPoint(SDL_Renderer r);
+void AfficherPoint(SDL_Renderer *r);
 void deplacementRectangle(SDL_Renderer * rend, SDL_Rect * rect, int x2, int y2); //déplace un rectangle dans l'espace en fonction des coordonnées de la souris
 SDL_Surface * loadImage(const char * file);
 SDL_Window * creerFenetre(const int w, const int h, const char * nom);
