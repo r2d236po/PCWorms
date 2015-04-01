11358b4f0266cb4fd19181a3b8f31f0b744968ff
orlyn44 <arrestif@gmail.com>
2015-04-01 10:37:45 +0200

    merge

--- TestSDL/Libraries.h
+++ TestSDL/Libraries.h
@@ -12,7 +12,7 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 #include <SDL/SDL_ttf.h>
-#include <SDL/SDL_mixer.h>
+//#include <SDL/SDL_mixer.h>
 
 /*Macros*/
 #define MY_ABS(a) ((a) < 0 ? (-a) : (a))
